b588cf62e32bcb9b06b510d9f35a992af441ac03
곽윤호 <kwakdm95@gmail.com>
2024-11-07 23:52:16 +0900

    react 기본 설정 react 기본 설정 빌드 스크립트 추가 빌드 파일 깃 추적 예외처리

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+import ReactDom from "react-dom/client";
+
+import Index from "./app/index";
+
+ReactDom.createRoot(document.getElementById("root")).render(<Index />);
---
+++ src/app/index.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <p>Hi! I'm react!</p>
+    </div>
+  )
+}
+export default Index;
